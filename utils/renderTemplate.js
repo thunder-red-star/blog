9a7b1c964b1b2c99eafa434a20278c2882fe3dc7
William <67127399+thunder-red-star@users.noreply.github.com>
2021-12-29 17:07:07 +0000

    repair more garbage code

--- utils/renderTemplate.js
+++ utils/renderTemplate.js
@@ -2,12 +2,10 @@ const path = require("path")
 
 module.exports = (req, res, template, data = {}) => {
 	const currentDir = path.resolve(`${process.cwd()}`);
-	const templateDir = path.resolve(currentDir.split("/")[file.split("/").length - 1] + path.sep + "templates")
+	const templateDir = path.resolve(currentDir.split("/")[currentDir.split("/").length - 1] + path.sep + "templates")
 
 	const baseData = {
-		bot: client,
 		path: req.path,
-		user: req.isAuthenticated() ? req.user : null
 	};
 
 	res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
