fb39732a1ca673d3444d95e496a8af11ad07cf2e
William <67127399+thunder-red-star@users.noreply.github.com>
2021-12-29 16:05:15 +0000

    reorder and add comments

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const express = require("express");
 const session = require("express-session");
 const ejs = require("ejs");
 
+// for ejs
 app.engine("html", ejs.renderFile);
 app.set("view engine", "html");
 
-app.use(bodyParser.json());
 app.use(express.static(__dirname + '/assets'));
+
+app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
 	extended: true
