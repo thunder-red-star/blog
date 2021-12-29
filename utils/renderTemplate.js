e907776667e9fe4b512acc7d3eeb7af16a07905b
William <67127399+thunder-red-star@users.noreply.github.com>
2021-12-29 17:02:09 +0000

    some templates

--- utils/renderTemplate.js
+++ utils/renderTemplate.js
@@ -1,10 +1,12 @@
 module.exports = (req, res, template, data = {}) => {
-	// Default base data which passed to the ejs template by default. 
+	const currentDir = path.resolve(`${process.cwd()}`);
+	const templateDir = path.resolve(currentDir.split("/")[file.split("/").length - 1] + path.sep + "templates")
+
 	const baseData = {
 		bot: client,
 		path: req.path,
 		user: req.isAuthenticated() ? req.user : null
 	};
-	// We render template using the absolute path of the template and the merged default data with the additional data provided.
+
 	res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
 };
