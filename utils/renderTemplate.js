0762fc651dd0872e7665e93d662d0b5abad8ba5e
William <67127399+thunder-red-star@users.noreply.github.com>
2021-12-29 17:13:06 +0000

    there was evidently no need to split

--- utils/renderTemplate.js
+++ utils/renderTemplate.js
@@ -2,11 +2,11 @@ const path = require("path")
 
 module.exports = (req, res, template, data = {}) => {
 	const currentDir = path.resolve(`${process.cwd()}`);
-	const templateDir = path.resolve(currentDir.split("/").slice(0, currentDir.split("/").length - 1) + path.sep + "templates")
+	const templateDir = path.resolve(currentDir + path.sep + "templates")
 
 	const baseData = {
 		path: req.path,
 	};
 
-	res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
+	res.render(path.resolve(`/${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
 };
