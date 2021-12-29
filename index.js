c82dd21587e7036cf37baccd5165cba4f6893843
William <67127399+thunder-red-star@users.noreply.github.com>
2021-12-29 16:18:04 +0000

    more uncategorized updates

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const ejs = require("ejs");
+const fs = require("fs");
+const term = require("terminal-kit").terminal;
+const bodyParser = require("body-parser")
+const app = express();
 
-// for ejs
+const config = require("./config/config.json")
+
+// for ejs rendering, fun!
 app.engine("html", ejs.renderFile);
 app.set("view engine", "html");
 
-app.use(express.static(__dirname + '/assets'));
+// serve static files like css/js/junk
+app.use(express.static(__dirname + '/static'));
 
+// for api usage
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
+const apiFiles = fs.readdirSync("api")
+
+for (endpoint of apiFiles) {
+	
+}
+
+app.listen(config.port, null, null, () => {
+	term.green("")
+});
